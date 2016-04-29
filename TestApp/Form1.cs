85482a79fd53d79f6762df02a66c625e48f240c9
valkrhlan <valkrhlan@foi.hr>
2016-04-30 00:45:42 +0200

    Vuki ima malu pišuuuu

--- TestApp/Form1.cs
+++ TestApp/Form1.cs
@@ -26,5 +26,11 @@ namespace TestApp
         {
             MessageBox.Show("Puši kurac");
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.Text = "Kurčić";
+            
+        }
     }
 }
