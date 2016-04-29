43011f404b463aa00c413090614281879ec62a7d
valkrhlan <valkrhlan@foi.hr>
2016-04-30 00:37:32 +0200

    Fuk ju

--- TestApp/Form1.cs
+++ TestApp/Form1.cs
@@ -16,5 +16,10 @@ namespace TestApp
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Crkni!");
+        }
     }
 }
